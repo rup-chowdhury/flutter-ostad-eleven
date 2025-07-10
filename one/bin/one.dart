52e860b161e18518ed8f9c047df806415fbaba28
Rup Chowdhury <rupbd1@gmail.com>
2025-07-10 23:25:58 +0600

    Update one.dart

--- one/bin/one.dart
+++ one/bin/one.dart
@@ -2,7 +2,7 @@ main() {
   String name = "Ruplaal";
   int number = 0;
   var number2 = 0;
-  bool isPracticedToday = false;
+  bool isPracticedToday = true;
 
   for(number2 = 0; number2 <= 10; number2++){
     print(number2);
