8486fc597be8a31e625735d15f68dcc668c9adb8
Rup Chowdhury <rupbd1@gmail.com>
2025-07-11 15:58:36 +0600

    Update module_1_class_2.dart

--- module_1_class_2/bin/module_1_class_2.dart
+++ module_1_class_2/bin/module_1_class_2.dart
@@ -5,6 +5,14 @@ main() {
   int age = 86;
   double height = 6.3;
   bool isAlive = true;
+
+  var test = 'test text';   //can be declared with any type of data. But can not be changed the type once declared
+  dynamic test2 = 'second test text'; //can be declared with any type of data. Also, data type of variable can be changed even after declaration
+  test2 = 50;
+  test2 = false;
   
   print('Name: $name \nAge: $age \nHeight: $height \nIs he still alive? - $isAlive');
+
+  print('var type variable: $test');
+  print('dynamic type variable: $test2');
 }
