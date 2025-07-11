db1913d7fd56a549f4056bf194dff099a4477ea7
Rup Chowdhury <rupbd1@gmail.com>
2025-07-11 16:36:28 +0600

    Class done

--- module_1_class_2/bin/module_1_class_2.dart
+++ module_1_class_2/bin/module_1_class_2.dart
@@ -6,8 +6,8 @@ main() {
   double height = 6.3;
   bool isAlive = true;
 
-  var test = 'test text';   //can be declared with any type of data. But can not be changed the type once declared
-  dynamic test2 = 'second test text'; //can be declared with any type of data. Also, data type of variable can be changed even after declaration
+  var test = 'test text';   //can be declared with any type of data. But can not be changed the type once declared. Changes during compile time.
+  dynamic test2 = 'second test text'; //can be declared with any type of data. Also, data type of variable can be changed even after declaration. Changes during runtime.
   test2 = 50;
   test2 = false;
   
@@ -15,4 +15,42 @@ main() {
 
   print('var type variable: $test');
   print('dynamic type variable: $test2');
+
+  //compile time. It can never be changed once declared.
+  const double PI = 3.1416;
+
+  //runtime. It can change during runtime
+  final DateTime currentDateTime = DateTime.now();
+
+  print(PI);
+  print(currentDateTime);
+
+  //check data type of variable
+  print(test.runtimeType);
+  print(test is double); //checking with condition
+
+
+  //+, -, *, /
+  int a = 20;
+  int b = 30;
+  print(a+b);
+  print(a-b);
+  print(a*b);
+  print(a/b);
+
+  //post increment
+  print(a++);
+  print(a);
+
+  //pre increment
+  print(++a);
+  print(a);
+
+  //post decrement
+  print(a--);
+  print(a);
+
+  //pre decrement
+  print(--a);
+  print(a);
 }
