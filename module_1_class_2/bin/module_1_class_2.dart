b88d88023c8615175dc66a603c85c3771636ac5e
Rup Chowdhury <rupbd1@gmail.com>
2025-07-11 15:38:09 +0600

    Update module_1_class_2.dart

--- module_1_class_2/bin/module_1_class_2.dart
+++ module_1_class_2/bin/module_1_class_2.dart
@@ -1,8 +1,10 @@
 main() {
-  print('Hello There \nGeneral Greivous');
+  // print('Hello There \nGeneral Greivous');
 
   String name = 'General Greivous';
   int age = 86;
   double height = 6.3;
   bool isAlive = true;
+  
+  print('Name: $name \nAge: $age \nHeight: $height \nIs he still alive? - $isAlive');
 }
