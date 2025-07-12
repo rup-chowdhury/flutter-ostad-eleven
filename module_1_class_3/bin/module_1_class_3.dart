9d2456db46e91007e283a278dbc00f87f9be27a9
Rup Chowdhury <rupbd1@gmail.com>
2025-07-12 19:35:43 +0600

    new day

---
+++ module_1_class_3/bin/module_1_class_3.dart
@@ -0,0 +1,5 @@
+import 'package:module_1_class_3/module_1_class_3.dart' as module_1_class_3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${module_1_class_3.calculate()}!');
+}
---
+++ module_1_class_3/test/module_1_class_3_test.dart
@@ -0,0 +1,8 @@
+import 'package:module_1_class_3/module_1_class_3.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
