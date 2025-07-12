5b030af970a1e0e3eea2555343b6e645bfcac269
Rup Chowdhury <rupbd1@gmail.com>
2025-07-12 19:37:05 +0600

    Update module_1_class_3.dart

--- module_1_class_3/bin/module_1_class_3.dart
+++ module_1_class_3/bin/module_1_class_3.dart
@@ -1,5 +1,3 @@
-import 'package:module_1_class_3/module_1_class_3.dart' as module_1_class_3;
+main () {
 
-void main(List<String> arguments) {
-  print('Hello world: ${module_1_class_3.calculate()}!');
 }
