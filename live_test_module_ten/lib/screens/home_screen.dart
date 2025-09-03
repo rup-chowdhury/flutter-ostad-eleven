8ec467e3ca017f5e7fe4760046a824b7485ad8df
Rup Chowdhury <rupbd1@gmail.com>
2025-09-03 16:04:59 +0200

    live test done

--- live_test_module_ten/lib/screens/home_screen.dart
+++ live_test_module_ten/lib/screens/home_screen.dart
@@ -6,7 +6,34 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text('Add Employee'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 0 ),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Name'
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Age'
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Salary'
+              ),
+            ),
+            SizedBox(height: 24,),
+            ElevatedButton(onPressed: (){},
+                child: Text('Add Employee'))
+          ],
+        ),
+      ),
     );
   }
 }
