1cde01b3bf474a1c017ad56696c770fd10f543e5
Rup Chowdhury <rupbd1@gmail.com>
2025-09-16 21:05:18 +0200

    new project

---
+++ module_2_class_1/bin/module_2_class_1.dart
@@ -0,0 +1,5 @@
+import 'package:module_2_class_1/module_2_class_1.dart' as module_2_class_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${module_2_class_1.calculate()}!');
+}
---
+++ module_2_class_1/test/module_2_class_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:module_2_class_1/module_2_class_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
