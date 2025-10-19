f4291af859a0decbc9136610ec698eb84c2cb9d5
Rup Chowdhury <rupbd1@gmail.com>
2025-10-19 22:07:11 +0200

    Splash Screen done

--- task_manager_basic/lib/ui/screens/splash_screen.dart
+++ task_manager_basic/lib/ui/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:task_manager_basic/ui/screens/login_screen.dart';
 import 'package:task_manager_basic/ui/utils/asset_path.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -11,6 +12,20 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    _moveToNextScreen();
+    super.initState();
+  }
+
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(Duration(seconds: 2));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (_) => LoginScreen()),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
@@ -21,7 +36,11 @@ class _SplashScreenState extends State<SplashScreen> {
             height: double.maxFinite,
             fit: BoxFit.cover,
           ),
-          Center(child: SvgPicture.asset(AssetPath.logoSvg, height: 50,))
+          Center(
+              child: SvgPicture.asset(
+            AssetPath.logoSvg,
+            height: 50,
+          ))
         ],
       ),
     );
