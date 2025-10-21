b67ec30c8b7cf056983a84970cd39feaf1da1cf4
Rup Chowdhury <rupbd1@gmail.com>
2025-10-21 23:27:51 +0200

    Add navigation to Forgot Password and Sign Up screens
    Introduces navigation to ForgotPasswordEmailVerificationScreen and SignUpScreen from the login screen. Refactors UI to use SingleChildScrollView, improves button styling, and disposes controllers in dispose().

--- task_manager_basic/lib/ui/screens/login_screen.dart
+++ task_manager_basic/lib/ui/screens/login_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:task_manager_basic/ui/screens/forgot_password_email_verification_screen.dart';
+import 'package:task_manager_basic/ui/screens/sign_up_screen.dart';
 import 'package:task_manager_basic/ui/widgets/screen_background.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -9,81 +12,118 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  final TextEditingController _emailTextEditingController = TextEditingController();
-  final TextEditingController _passwordTextEditingController = TextEditingController();
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ScreenBackground(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 82,
-                ),
-                Text(
-                  'Get Started With',
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.w600,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 82,
                   ),
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                TextFormField(
-                  controller: _emailTextEditingController,
-                  decoration: InputDecoration(
-                    hintText: 'Email'
-                  )
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                TextFormField(
-                  controller: _passwordTextEditingController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Password'
+                  Text(
+                    'Get Started With',
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                FilledButton(
-                  style: FilledButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    fixedSize: Size.fromWidth(double.maxFinite),
-                    padding: EdgeInsets.symmetric(vertical: 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  TextFormField(
+                      controller: _emailTextEditingController,
+                      decoration: InputDecoration(hintText: 'Email')),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  TextFormField(
+                    controller: _passwordTextEditingController,
+                    obscureText: true,
+                    decoration: InputDecoration(hintText: 'Password'),
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  FilledButton(
+                      onPressed: () {},
+                      child: Icon(Icons.arrow_circle_right_outlined)),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Center(
+                    child: Column(
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              _onTapForgotPasswordButton();
+                            },
+                            child: Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),
+                            )),
+                        RichText(
+                          text: TextSpan(
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600),
+                              text: "Don't have an account? ",
+                              children: [
+                                TextSpan(
+                                  text: "Sign Up",
+                                  style: TextStyle(
+                                    color: Colors.green,
+                                  ),
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = _onTapSignUpButton,
+                                )
+                              ]),
+                        ),
+                      ],
                     ),
                   ),
-                    
-                    onPressed: () {},
-                    child: Icon(Icons.arrow_circle_right_outlined)),
-                TextButton(onPressed: () {}, child: Text('Forgot Password?')),
-                RichText(
-                  text: TextSpan(
-                      style: TextStyle(color: Colors.black),
-                      text: "Don't have an account?",
-                      children: [
-                        TextSpan(
-                          text: "Sign Up",
-                        )
-                      ]),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  void _onTapForgotPasswordButton() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ForgotPasswordEmailVerificationScreen(),
+      ),
+    );
+  }
+
+  void _onTapSignUpButton() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SignUpScreen(),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailTextEditingController.dispose();
+    _passwordTextEditingController.dispose();
+    super.dispose();
   }
 }
