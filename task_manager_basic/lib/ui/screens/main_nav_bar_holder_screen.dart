8c3a339bc746c559b7ec110fd289a5efd6e21aea
Rup Chowdhury <rupbd1@gmail.com>
2025-10-28 23:40:45 +0200

    .

---
+++ task_manager_basic/lib/ui/screens/main_nav_bar_holder_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainNavBarHolderScreen extends StatefulWidget {
+  const MainNavBarHolderScreen({super.key});
+
+  @override
+  State<MainNavBarHolderScreen> createState() => _MainNavBarHolderScreenState();
+}
+
+class _MainNavBarHolderScreenState extends State<MainNavBarHolderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
