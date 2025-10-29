b56bf708359670391f9e21064fb0c46dd677d1f2
Rup Chowdhury <rupbd1@gmail.com>
2025-10-29 23:51:34 +0200

    Update main_nav_bar_holder_screen.dart

--- task_manager_basic/lib/ui/screens/main_nav_bar_holder_screen.dart
+++ task_manager_basic/lib/ui/screens/main_nav_bar_holder_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_basic/ui/screens/progress_task_screen.dart';
+import 'package:task_manager_basic/ui/widgets/tm_app_bar.dart';
 
 class MainNavBarHolderScreen extends StatefulWidget {
   const MainNavBarHolderScreen({super.key});
@@ -10,10 +12,18 @@ class MainNavBarHolderScreen extends StatefulWidget {
 class _MainNavBarHolderScreenState extends State<MainNavBarHolderScreen> {
   int _selectedIndex = 0;
 
+  final List<Widget> _screens = [
+    ProgressTaskScreen(),
+    ProgressTaskScreen(),
+    ProgressTaskScreen(),
+    ProgressTaskScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: TMAppBar(),
+      body: _screens[_selectedIndex],
       bottomNavigationBar: NavigationBar(
           selectedIndex: _selectedIndex,
           onDestinationSelected: (int index) {
@@ -21,23 +31,25 @@ class _MainNavBarHolderScreenState extends State<MainNavBarHolderScreen> {
             setState(() {});
           },
           destinations: [
-        NavigationDestination(
-          icon: Icon(Icons.new_label_outlined),
-          label: 'New',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.refresh),
-          label: 'Progress',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.close),
-          label: 'Canceled',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.done),
-          label: 'Completed',
-        ),
-      ]),
+            NavigationDestination(
+              icon: Icon(Icons.new_label_outlined),
+              label: 'New',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.refresh),
+              label: 'Progress',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.close),
+              label: 'Canceled',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.done),
+              label: 'Completed',
+            ),
+          ]),
     );
   }
 }
+
+
