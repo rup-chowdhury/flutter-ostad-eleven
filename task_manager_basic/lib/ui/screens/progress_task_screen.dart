8d17024a0397d8b6d66b1da132b766931786f555
Rup Chowdhury <rupbd1@gmail.com>
2025-10-30 02:16:14 +0200

    Update progress_task_screen.dart

--- task_manager_basic/lib/ui/screens/progress_task_screen.dart
+++ task_manager_basic/lib/ui/screens/progress_task_screen.dart
@@ -12,6 +12,57 @@ class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(onPressed: (){}, child: Icon(Icons.add),),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            SizedBox(height: 16,),
+            SizedBox(
+              height: 100,
+              child: ListView.separated(
+                itemCount: 4,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return TaskCountByStatusCard(
+                    title: 'New',
+                    count: 2,
+                  );
+                }, separatorBuilder: (context, index) {
+                  return SizedBox(width: 8,);
+              },
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class TaskCountByStatusCard extends StatelessWidget {
+  const TaskCountByStatusCard({
+    super.key, required this.title, required this.count,
+  });
+
+  final String title;
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+        child: Column(
+          children: [
+            Text('$count', style: Theme.of(context).textTheme.titleLarge,),
+            Text(title, style: TextStyle(
+              color: Colors.grey,
+            ),),
+          ],
+        ),
+      ),
     );
   }
 }
