364a1c71b7bf85a3766ec76a658b044ee892c0e6
Rup Chowdhury <rupbd1@gmail.com>
2025-10-31 17:51:09 +0200

    Make app bar profile section navigable
    Wraps the app bar's profile info in a GestureDetector to allow navigation to the UpdateProfileScreen when tapped. This improves user experience by providing a direct way to access profile updates from the app bar.

--- task_manager_basic/lib/ui/widgets/tm_app_bar.dart
+++ task_manager_basic/lib/ui/widgets/tm_app_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager_basic/ui/screens/update_profile_screen.dart';
 
-class TMAppBar extends StatelessWidget implements PreferredSizeWidget{
+class TMAppBar extends StatelessWidget implements PreferredSizeWidget {
   const TMAppBar({
     super.key,
   });
@@ -9,37 +10,47 @@ class TMAppBar extends StatelessWidget implements PreferredSizeWidget{
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: Colors.green,
-      title: Row(
-        spacing: 8,
-        children: [
-          CircleAvatar(),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Full Name",
-                style: Theme.of(context)
-                    .textTheme
-                    .titleMedium
-                    ?.copyWith(color: Colors.white),
-              ),
-              Text(
-                "rupbd1@gmail.com",
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium
-                    ?.copyWith(color: Colors.white),
-              ),
-            ],
-          )
-        ],
+      title: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => UpdateProfileScreen(),
+            ),
+          );
+        },
+        child: Row(
+          spacing: 8,
+          children: [
+            CircleAvatar(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Full Name",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(color: Colors.white),
+                ),
+                Text(
+                  "rupbd1@gmail.com",
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium
+                      ?.copyWith(color: Colors.white),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
       actions: [
-        IconButton(onPressed: (){}, icon: Icon(Icons.logout)),
+        IconButton(onPressed: () {}, icon: Icon(Icons.logout)),
       ],
     );
   }
+
   @override
   Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
-
