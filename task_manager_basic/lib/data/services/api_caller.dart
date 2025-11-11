2882561e77ada192ea37344ed7634b06d25985b9
Rup Chowdhury <rupbd1@gmail.com>
2025-11-11 12:36:45 +0200

    get response from the API

--- task_manager_basic/lib/data/services/api_caller.dart
+++ task_manager_basic/lib/data/services/api_caller.dart
@@ -1,3 +1,47 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class ApiCaller {
+  Future<ApiResponse> getRequest({required String url}) async {
+    try {
+      Uri uri = Uri.parse(url);
+      Response response = await get(uri);
+
+      print(url);
+      print(response.statusCode);
+      print(response.body);
+
+      final int statusCode = response.statusCode;
+
+      if (response.statusCode == 200) {
+        //SUCCESS
+        final decodedData = jsonDecode(response.body);
+        return ApiResponse(
+          isSuccess: true,
+          responseCode: statusCode,
+          responseData: decodedData,
+        );
+      } else {
+        //FAILURE
+        final decodedData = jsonDecode(response.body);
+        return ApiResponse(
+          isSuccess: false,
+          responseCode: statusCode,
+          responseData: decodedData,
+        );
+      }
+    } on Exception catch (e) {
+      return ApiResponse(
+        isSuccess: false,
+        responseCode: -1,
+        responseData: null,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+}
+
 class ApiResponse {
   final bool isSuccess;
   final int responseCode;
