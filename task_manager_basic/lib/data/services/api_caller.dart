68fe86189089ab0ed943f159ff48b3de0f0e8279
Rup Chowdhury <rupbd1@gmail.com>
2025-11-11 11:50:33 +0200

    ApiResponse to stop http package exposure

---
+++ task_manager_basic/lib/data/services/api_caller.dart
@@ -0,0 +1,13 @@
+class ApiResponse {
+  final bool isSuccess;
+  final int responseCode;
+  final dynamic responseData;
+  final String? errorMessage;
+
+  ApiResponse({
+    required this.isSuccess,
+    required this.responseCode,
+    required this.responseData,
+    this.errorMessage,
+  });
+}
