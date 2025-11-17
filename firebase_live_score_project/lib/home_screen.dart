68f262d944d3ee2a918a9abcfaedb8d0f367e23a
Rup Chowdhury <rupbd1@gmail.com>
2025-11-17 20:26:45 +0200

    retrieve result from firestore

--- firebase_live_score_project/lib/home_screen.dart
+++ firebase_live_score_project/lib/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,20 +10,71 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<CricketMatch> _matchList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getCricketMatches();
+  }
+
+  Future<void> _getCricketMatches() async {
+    _matchList.clear();
+    final snapshots = await FirebaseFirestore.instance
+        .collection('cricket')
+        .get();
+    for (QueryDocumentSnapshot<Map<String, dynamic>> doc in snapshots.docs) {
+      _matchList.add(
+        CricketMatch(
+          id: doc.id,
+          team1: doc.get('team1'),
+          team1Score: doc.get('team1_score'),
+          team2: doc.get('team2'),
+          team2Score: doc.get('team2_score'),
+          isRunning: doc.get('is_running'),
+          winner: doc.get('winner_team'),
+        ),
+      );
+    }
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Live score'),
+      appBar: AppBar(title: Text('Live score')),
+      body: ListView.builder(
+        itemCount: _matchList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(backgroundColor: Colors.green, radius: 12),
+            title: Text('Bangladesh vs England'),
+            trailing: Text('12 - 14'),
+            subtitle: Text('Winner: '),
+          );
+        },
       ),
-      body: ListView.builder(itemBuilder: (context, index) {
-        return ListTile(
-          leading: CircleAvatar(backgroundColor: Colors.green, radius: 12,),
-          title: Text('Bangladesh vs England'),
-          trailing: Text('12 - 14'),
-          subtitle: Text('Winner: '),
-        );
-      }),
     );
   }
+}
+
+class CricketMatch {
+  final String id;
+  final String team1;
+  final int team1Score;
+  final String team2;
+  final int team2Score;
+  final bool isRunning;
+  final String winner;
+
+  CricketMatch({
+    required this.id,
+    required this.team1,
+    required this.team1Score,
+    required this.team2,
+    required this.team2Score,
+    required this.isRunning,
+    required this.winner,
+  });
 }
