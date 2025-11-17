6d1ee9a24bf3ecd7ccf955ce023796d3628e3c83
Rup Chowdhury <rupbd1@gmail.com>
2025-11-17 21:28:13 +0200

    getting dynamic data from firestore

--- firebase_live_score_project/lib/home_screen.dart
+++ firebase_live_score_project/lib/home_screen.dart
@@ -14,56 +14,70 @@ class _HomeScreenState extends State<HomeScreen> {
   bool _inProgress = false;
 
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _getCricketMatches();
-  }
-
-  Future<void> _getCricketMatches() async {
-    _inProgress = true;
-    setState(() {});
-    _matchList.clear();
-    final snapshots = await FirebaseFirestore.instance
-        .collection('cricket')
-        .get();
-    for (QueryDocumentSnapshot<Map<String, dynamic>> doc in snapshots.docs) {
-      _matchList.add(
-        CricketMatch(
-          id: doc.id,
-          team1: doc.get('team1'),
-          team1Score: doc.get('team1_score'),
-          team2: doc.get('team2'),
-          team2Score: doc.get('team2_score'),
-          isRunning: doc.get('is_running'),
-          winner: doc.get('winner_team'),
-        ),
-      );
-    }
-    _inProgress = false;
-    setState(() {});
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _getCricketMatches();
+  // }
+  //
+  // Future<void> _getCricketMatches() async {
+  //   _inProgress = true;
+  //   setState(() {});
+  //   _matchList.clear();
+  //   final snapshots = await FirebaseFirestore.instance
+  //       .collection('cricket')
+  //       .get();
+  //   for (QueryDocumentSnapshot<Map<String, dynamic>> doc in snapshots.docs) {
+  //     _matchList.add(
+  //       CricketMatch(
+  //         id: doc.id,
+  //         team1: doc.get('team1'),
+  //         team1Score: doc.get('team1_score'),
+  //         team2: doc.get('team2'),
+  //         team2Score: doc.get('team2_score'),
+  //         isRunning: doc.get('is_running'),
+  //         winner: doc.get('winner_team'),
+  //       ),
+  //     );
+  //   }
+  //   _inProgress = false;
+  //   setState(() {});
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Live score')),
-      body: Visibility(
-        visible: _inProgress == false,
-        replacement: Center(child: CircularProgressIndicator()),
-        child: ListView.builder(
-          itemCount: _matchList.length,
-          itemBuilder: (context, index) {
-            final cricketMatch = _matchList[index];
-            return ListTile(
-              leading: CircleAvatar(backgroundColor: cricketMatch.isRunning ? Colors.green : Colors.red, radius: 12),
-              title: Text('${cricketMatch.team1} vs ${cricketMatch.team2}'),
-              trailing: Text('${cricketMatch.team1Score} - ${cricketMatch.team2Score}'),
-              subtitle: Text(cricketMatch.isRunning ? "Pending . . ." :'Winner: ${cricketMatch.winner}'),
-            );
-          },
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('cricket').snapshots(),
+        builder: (context, snapshots) {
+            for (QueryDocumentSnapshot<Map<String, dynamic>> doc in snapshots.data!.docs) {
+              _matchList.add(
+                CricketMatch(
+                  id: doc.id,
+                  team1: doc.get('team1'),
+                  team1Score: doc.get('team1_score'),
+                  team2: doc.get('team2'),
+                  team2Score: doc.get('team2_score'),
+                  isRunning: doc.get('is_running'),
+                  winner: doc.get('winner_team'),
+                ),
+              );
+            }
+
+          return ListView.builder(
+            itemCount: _matchList.length,
+            itemBuilder: (context, index) {
+              final cricketMatch = _matchList[index];
+              return ListTile(
+                leading: CircleAvatar(backgroundColor: cricketMatch.isRunning ? Colors.green : Colors.red, radius: 12),
+                title: Text('${cricketMatch.team1} vs ${cricketMatch.team2}'),
+                trailing: Text('${cricketMatch.team1Score} - ${cricketMatch.team2Score}'),
+                subtitle: Text(cricketMatch.isRunning ? "Pending . . ." :'Winner: ${cricketMatch.winner}'),
+              );
+            },
+          );
+        }
       ),
     );
   }
