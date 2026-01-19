c4627d05ecea5546a1413617a7f1c9ef9f751dd3
Rup Chowdhury <rupbd1@gmail.com>
2026-01-19 22:54:41 +0200

    Create app.dart

---
+++ e_commerce/lib/app/app.dart
@@ -0,0 +1,24 @@
+import 'package:e_commerce/app/app_routes.dart';
+import 'package:e_commerce/features/auth/presentation/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class CraftyBayApp extends StatefulWidget {
+  const CraftyBayApp({super.key});
+
+  @override
+  State<CraftyBayApp> createState() => _CraftyBayAppState();
+}
+
+class _CraftyBayAppState extends State<CraftyBayApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // router
+      // theme
+      initialRoute: SplashScreen.name,
+      onGenerateRoute: AppRoutes.routes,
+      theme: ThemeData(),
+      darkTheme: ThemeData(),
+    );
+  }
+}
