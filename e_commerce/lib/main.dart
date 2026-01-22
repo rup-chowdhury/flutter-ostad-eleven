d81658d62a0bebf26b7a2e212085300ad760164d
Rup Chowdhury <rupbd1@gmail.com>
2026-01-23 01:09:50 +0200

    .

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -8,9 +8,23 @@
 // 7. Routing
 // 8. Network Caller
 
+import 'dart:async';
+
 import 'package:e_commerce/app/app.dart';
+import 'package:e_commerce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(CraftyBayApp());
+void main() async {
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    // The following lines are the same as previously explained in "Handling uncaught errors"
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+    runApp(CraftyBayApp());
+  }, (error, stack) => FirebaseCrashlytics.instance.recordError(error, stack));
 }
