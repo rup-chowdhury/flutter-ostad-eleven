4bddaf97345acf94816d902a42c5ab0a88e262ed
Rup Chowdhury <rupbd1@gmail.com>
2026-01-26 23:41:17 +0200

    Provider start

--- e_commerce/lib/app/app.dart
+++ e_commerce/lib/app/app.dart
@@ -1,9 +1,11 @@
 import 'package:e_commerce/app/app_routes.dart';
 import 'package:e_commerce/app/app_theme.dart';
+import 'package:e_commerce/app/providers/language_provider.dart';
 import 'package:e_commerce/features/auth/presentation/screens/splash_screen.dart';
 import 'package:e_commerce/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 
 class CraftyBayApp extends StatefulWidget {
   const CraftyBayApp({super.key});
@@ -15,26 +17,35 @@ class CraftyBayApp extends StatefulWidget {
 class _CraftyBayAppState extends State<CraftyBayApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // router
-      // theme
-      initialRoute: SplashScreen.name,
-      onGenerateRoute: AppRoutes.routes,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.light,
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => LanguageProvider()),
       ],
-      supportedLocales: [
-        Locale('en'),
-        Locale('bn'),
-      ],
-      locale: Locale('bn'),
+      child: Consumer<LanguageProvider>(
+        builder: (context, languageProvider, child) {
+          return MaterialApp(
+            // router
+            // theme
+            initialRoute: SplashScreen.name,
+            onGenerateRoute: AppRoutes.routes,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: ThemeMode.light,
+            localizationsDelegates: [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: [
+              Locale('en'),
+              Locale('bn'),
+            ],
+            locale: languageProvider.currentLocale,
 
+          );
+        }
+      ),
     );
   }
 }
---
+++ e_commerce/lib/app/providers/language_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LanguageProvider extends ChangeNotifier{
+  Locale _currentLocale = Locale('en');
+
+  Locale get currentLocale => _currentLocale;
+
+  void changeLocale(Locale newLocale) {
+    if (_currentLocale == newLocale) return;
+
+    _currentLocale = newLocale;
+    notifyListeners();
+  }
+}
