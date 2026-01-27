95a51f6055119c4e4a7cfdebda5316029f63d058
Rup Chowdhury <rupbd1@gmail.com>
2026-01-27 23:18:27 +0200

    Update language_provider.dart

--- e_commerce/lib/app/providers/language_provider.dart
+++ e_commerce/lib/app/providers/language_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class LanguageProvider extends ChangeNotifier{
+class LanguageProvider extends ChangeNotifier {
   Locale _currentLocale = Locale('en');
 
   Locale get currentLocale => _currentLocale;
